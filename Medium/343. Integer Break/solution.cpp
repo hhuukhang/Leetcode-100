21bd5394e8c38567879ca3096368601108bc1695
Hoàng Hữu Khang <huukhanghoang2@gmail.com>
2026-01-16 15:23:03 +0700

    ..

---
+++ Medium/343. Integer Break/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        if (n % 3 == 1) return pow(3, n / 3 - 1) * 4;
+        if (!(n % 3)) return pow(3, n / 3);
+        return pow (3, n / 3) * (n % 3);
+    }
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+# 🚀 My 100 LeetCode Challenge (70 Medium - 30 Hard)
+
+Tiến độ: [2%] 2/100
+
